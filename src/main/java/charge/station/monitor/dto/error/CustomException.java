40d60fa71c6dbcd623478798589c172366df519a
banjinseong <banjinseong@gmail.com>
2025-03-24 10:23:34 +0900

    로그추가

--- src/main/java/charge/station/monitor/dto/error/CustomException.java
+++ src/main/java/charge/station/monitor/dto/error/CustomException.java
@@ -2,8 +2,10 @@ package charge.station.monitor.dto.error;
 
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
+@Slf4j
 @Getter
 public class CustomException extends RuntimeException {
     private final HttpStatus status;
@@ -11,5 +13,6 @@ public class CustomException extends RuntimeException {
     public CustomException(String message, HttpStatus status) {
         super(message);
         this.status = status;
+        log.error("예외 발생: {} (HTTP 상태: {})", message, status);
     }
 }
