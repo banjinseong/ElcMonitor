73803bdf8816f0a76f63a3390de56a94ef14336b
banjinseong <banjinseong@gmail.com>
2025-03-28 15:33:40 +0900

    getter추가

--- src/main/java/charge/station/monitor/config/auth/CustomUserDetails.java
+++ src/main/java/charge/station/monitor/config/auth/CustomUserDetails.java
@@ -1,14 +1,17 @@
 package charge.station.monitor.config.auth;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 public class CustomUserDetails extends org.springframework.security.core.userdetails.User {
 
+    // ✅ userId 가져오기
     private Long userId;  // ✅ userId 추가
-    private List<String> managedRegions;
+    private final List<String> managedRegions;
 
     public CustomUserDetails(Long userId, String username, String password, Collection<? extends GrantedAuthority> authorities,
                              List<String> managedRegions) {
@@ -16,11 +19,4 @@ public class CustomUserDetails extends org.springframework.security.core.userdet
         this.managedRegions = managedRegions;
     }
 
-    public Long getUserId() {  // ✅ userId 가져오기
-        return userId;
-    }
-
-    public List<String> getManagedRegions() {
-        return managedRegions;
-    }
 }
