cd5c3e102d9f6a365d08ca63aa03cfa757344ef7
banjinseong <banjinseong@gmail.com>
2025-07-07 11:36:57 +0900

    테스트용 builder추가

--- src/main/java/charge/station/monitor/dto/user/JoinRequestDTO.java
+++ src/main/java/charge/station/monitor/dto/user/JoinRequestDTO.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class JoinRequestDTO {
 
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
